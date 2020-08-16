be5ef04c0c124b2e42033d2d0e092be10e3cc964
mikeyrayvon <mikey@interglobal.vision>
2020-08-16 16:53:42 +0200

    add header to index

---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Header() {
+  return <h1>Site Title</h1>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Header />
+      <div>Hello World!</div>
+    </>
+  )
 }
